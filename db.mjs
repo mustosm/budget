4c37b4bd1ee0ab60389dcf1333ef3e83c3213a91
must <mustapha.benosmane@gmail.com>
2017-11-25 11:45:09 +0100

    Ajout de @std/esm

--- db.mjs
+++ db.mjs
@@ -1,3 +1,8 @@
 // db.js
 import Mongoose from 'mongoose';
-Mongoose.connect('mongodb://must:110986@ds111885.mlab.com:11885/monogodb-db');
+Mongoose.connect('mongodb://must:110986@ds111885.mlab.com:11885/monogodb-db',
+    {
+        useMongoClient: true
+    });
+
+export default Mongoose;
